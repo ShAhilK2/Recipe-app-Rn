1354829094af69e3e5770baf2d3f49267c033c22
ShAhilK2 <shahil.kataria@invennico.com>
2025-06-23 00:01:04 +0530

    feat: 04:delete favorite recipe

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import { ENV } from "./config/env.js";
 import db from "./db/db.js";
 import { favoritesTable } from "./db/schema.js";
+import { and, eq } from "drizzle-orm";
 const app = express();
 
 const PORT = ENV.PORT || 5001;
@@ -30,6 +31,21 @@ app.post("/api/favorites", async (req, res) => {
     res.status(500).json({ error: "Something went wrong " + err.message });
   }
 });
+
+
+app.delete("/api/favorites/:userId/:recipeId",async(req,res)=>{
+    try{
+
+        const {userId,recipeId} = req.params;
+        await db.delete(favoritesTable).where(and(eq(favoritesTable.userId,userId),eq(favoritesTable.recipeId,parseInt(recipeId))));
+        res.status(200).json({message : "Favorite removed successfully"});
+        
+    }catch(error){
+        console.log(error);
+        res.status(500).json({ error : "Something went wrong " + error.message})
+
+    }
+}) 
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
