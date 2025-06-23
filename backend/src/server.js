3f687393fc52592607971f487084e3a2cedd51cb
ShAhilK2 <shahil.kataria@invennico.com>
2025-06-23 23:17:19 +0530

    feat: 06:Cronjob added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,15 @@ import { ENV } from "./config/env.js";
 import db from "./db/db.js";
 import { favoritesTable } from "./db/schema.js";
 import { and, eq } from "drizzle-orm";
+import cronJob from "./config/cron.js";
 const app = express();
 
 const PORT = ENV.PORT || 5001;
 app.use(express.json());
+
+if (ENV.NODE_ENV === "production") {
+  cronJob.start();
+}
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ success: true });
